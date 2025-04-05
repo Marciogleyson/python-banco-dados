21d898403a6351e17993860d1724796d5175d323
Marcio Ramos <marcio.gleyson2026@gmail.com>
2025-04-04 21:40:18 -0300

    criado tabela pelo python

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-if __name__ == "__main__:":
-    print("Olá Mundo!!")
+from mysql.connector import connect
+
+if __name__ == "__main__":
+    print("Tentando conectar o banco de dados!!")
+
+    conexao = connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="admin",
+        database="super_empresa"
+    )
+    print("Conexão realizada com sucesso")
+    #Criar um cursor que nos permitirá executar comandos  no banco de dados
+    cursor = conexao.cursor()
+    # Definir qual será o comando que iremos executar, neste caso será um insert
+    cursor.execute("insert into produtos (nome) values ('X-calabresa')")
+    cursor.execute("insert into produtos (nome) values ('X-Bacon')")
+
+    # Commit é nescessário pois sem ele o insert n será concretizado no bd
+    conexao.commit()
+    # Fechar a conexão com o db
+    conexao.close()
+    print("Produto cadastrado com sucesso")
